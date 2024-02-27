12eae5afc264b162ca6fe614b632a942f0b9a204
Mirgo25 <mirgo2513@gmail.com>
2024-02-27 23:04:08 +0200

    added Journal Item 1

---
+++ src/components/JournalItem/JournalItem.jsx
@@ -0,0 +1,19 @@
+import './JournalItem.css';
+
+function JournalItem() {
+  const title = 'Some Header!';
+  const date = new Date();
+  const text = 'lorem ipsum some text alskdhjkashd ajshd dasd.';
+
+  return (
+    <div className="journal-item">
+      <h2 className="journal-item__header">{title}</h2>
+      <h2 className="journal-item__body">
+        <div className="journal-item__date">{date.toString()}</div>
+        <div className="journal-item__text">{text}</div>
+      </h2>
+    </div>
+  );
+}
+
+export default JournalItem;
