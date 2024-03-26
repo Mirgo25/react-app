9e65e80c38a9173da2634144a6192c58c1d133f1
Mirgo25 <mirgo2513@gmail.com>
2024-03-26 15:28:04 +0200

    fix indentations

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,35 @@ import Button from './components/Button/Button';
 import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
-  const data = [
-    {
-      title: 'Title #1',
-      date: new Date(),
-      text: 'Some text 1.'
-    },
-    {
-      title: 'Title #2',
-      date: new Date(),
-      text: 'Some text 2.'
-    }
-  ];
-  return (
-    <>
-      <h1>Header</h1>
-      <p>Some text</p>
-      <Button />
-      <JournalItem
-        title={data[0].title}
-        date={data[0].date}
-        text={data[0].text}
-      />
-      <JournalItem
-        title={data[1].title}
-        date={data[1].date}
-        text={data[1].text}
-      />
-    </>
-  );
+	const data = [
+		{
+			title: 'Title #1',
+			date: new Date(),
+			text: 'Some text 1.'
+		},
+		{
+			title: 'Title #2',
+			date: new Date(),
+			text: 'Some text 2.'
+		}
+	];
+	return (
+		<>
+			<h1>Header</h1>
+			<p>Some text</p>
+			<Button />
+			<JournalItem
+				title={data[0].title}
+				date={data[0].date}
+				text={data[0].text}
+			/>
+			<JournalItem
+				title={data[1].title}
+				date={data[1].date}
+				text={data[1].text}
+			/>
+		</>
+	);
 }
 
 export default App;
--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,17 +1,17 @@
 import './JournalItem.css';
 
 function JournalItem({ title, date, text }) {
-  const formatedDate = new Intl.DateTimeFormat('ua-UA').format(date);
+	const formatedDate = new Intl.DateTimeFormat('ua-UA').format(date);
 
-  return (
-    <div className="journal-item">
-      <h2 className="journal-item__header">{title}</h2>
-      <h2 className="journal-item__body">
-        <div className="journal-item__date">{formatedDate}</div>
-        <div className="journal-item__text">{text}</div>
-      </h2>
-    </div>
-  );
+	return (
+		<div className="journal-item">
+			<h2 className="journal-item__header">{title}</h2>
+			<h2 className="journal-item__body">
+				<div className="journal-item__date">{formatedDate}</div>
+				<div className="journal-item__text">{text}</div>
+			</h2>
+		</div>
+	);
 }
 
 export default JournalItem;
