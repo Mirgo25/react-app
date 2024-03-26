743c9b998184b9181f627e17e10c6db2e78ce174
Mirgo25 <mirgo2513@gmail.com>
2024-03-26 19:46:06 +0200

    website layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
 import Button from './components/Button/Button';
 import CardButton from './components/CardButton/CardButton';
+import Header from './components/Header/Header';
+import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalItem from './components/JournalItem/JournalItem';
+import JournalList from './components/JournalList/JournalList';
+import Body from './layouts/Body/Body';
+import LeftPanel from './layouts/LeftPanel/LeftPanel';
 
 function App() {
 	const data = [
@@ -17,26 +22,29 @@ function App() {
 		}
 	];
 	return (
-		<>
-			<h1>Header</h1>
-			<p>Some text</p>
-			<Button />
-			<CardButton>New memory</CardButton>
-			<CardButton>
-				<JournalItem
-					title={data[0].title}
-					date={data[0].date}
-					text={data[0].text}
-				/>
-			</CardButton>
-			<CardButton>
-				<JournalItem
-					title={data[1].title}
-					date={data[1].date}
-					text={data[1].text}
-				/>
-			</CardButton>
-		</>
+		<div className="app">
+			<LeftPanel>
+				<Header />
+				<JournalAddButton />
+				<JournalList>
+					<CardButton>
+						<JournalItem
+							title={data[0].title}
+							date={data[0].date}
+							text={data[0].text}
+						/>
+					</CardButton>
+					<CardButton>
+						<JournalItem
+							title={data[1].title}
+							date={data[1].date}
+							text={data[1].text}
+						/>
+					</CardButton>
+				</JournalList>
+			</LeftPanel>
+			<Body>Body</Body>
+		</div>
 	);
 }
 
---
+++ src/components/JournalAddButton/JournalAddButton.jsx
@@ -0,0 +1,8 @@
+import CardButton from '../CardButton/CardButton';
+import './JournalAddButton.css';
+
+function JournalAddButton() {
+	return <CardButton className="journal-add">New memory</CardButton>;
+}
+
+export default JournalAddButton;
