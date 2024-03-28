2bd8e7c9085e58d7e54287603cf97e3b805a7264
Mirgo25 <mirgo2513@gmail.com>
2024-03-28 19:16:31 +0200

    added returning the jsx by condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
 import { useState } from 'react';
 import './App.css';
-import CardButton from './components/CardButton/CardButton';
 import Header from './components/Header/Header';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalForm from './components/JournalForm/JournalForm';
-import JournalItem from './components/JournalItem/JournalItem';
 import JournalList from './components/JournalList/JournalList';
 import Body from './layouts/Body/Body';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 
 const INITIAL_DATA = [
-	{
-		id: 1,
-		title: 'Title #1',
-		date: new Date(),
-		text: 'Some text 1.'
-	},
-	{
-		id: 2,
-		title: 'Title #2',
-		date: new Date(),
-		text: 'Some text 2.'
-	}
+	// {
+	// 	id: 1,
+	// 	title: 'Title #1',
+	// 	date: new Date(),
+	// 	text: 'Some text 1.'
+	// },
+	// {
+	// 	id: 2,
+	// 	title: 'Title #2',
+	// 	date: new Date(),
+	// 	text: 'Some text 2.'
+	// }
 ];
 
 function App() {
@@ -38,23 +36,14 @@ function App() {
 		]);
 	};
 
-	const sortedItems = (a, b) => {
-		if (a.date < b.date) return 1;
-		return -1;
-	};
+
 
 	return (
 		<div className="app">
 			<LeftPanel>
 				<Header />
 				<JournalAddButton />
-				<JournalList>
-					{items.sort(sortedItems).map((el) => (
-						<CardButton key={el.id}>
-							<JournalItem title={el.title} date={el.date} text={el.text} />
-						</CardButton>
-					))}
-				</JournalList>
+				<JournalList items={items} />
 			</LeftPanel>
 			<Body>
 				<JournalForm onSubmit={addItem} />
--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,7 +1,24 @@
+import CardButton from '../CardButton/CardButton';
+import JournalItem from '../JournalItem/JournalItem';
 import './JournalList.css';
 
-function JournalList({ children }) {
-	return <div className="journal-list">{children}</div>;
+function JournalList({ items }) {
+	if (!items.length) return <p>There are no items yet. Add some</p>;
+
+	const sortItems = (a, b) => {
+		if (a.date < b.date) return 1;
+		return -1;
+	};
+
+	return (
+		<>
+			{items.sort(sortItems).map((el) => (
+				<CardButton key={el.id}>
+					<JournalItem title={el.title} date={el.date} text={el.text} />
+				</CardButton>
+			))}
+		</>
+	);
 }
 
 export default JournalList;
