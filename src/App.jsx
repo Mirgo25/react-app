ce128899704e8deef26941c10033a7fda557fcde
Mirgo25 <mirgo2513@gmail.com>
2024-03-28 18:44:18 +0200

    added keys to list of items

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import CardButton from './components/CardButton/CardButton';
 import Header from './components/Header/Header';
@@ -8,43 +9,55 @@ import JournalList from './components/JournalList/JournalList';
 import Body from './layouts/Body/Body';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 
+const INITIAL_DATA = [
+	{
+		id: 1,
+		title: 'Title #1',
+		date: new Date(),
+		text: 'Some text 1.'
+	},
+	{
+		id: 2,
+		title: 'Title #2',
+		date: new Date(),
+		text: 'Some text 2.'
+	}
+];
+
 function App() {
-	const data = [
-		{
-			title: 'Title #1',
-			date: new Date(),
-			text: 'Some text 1.'
-		},
-		{
-			title: 'Title #2',
-			date: new Date(),
-			text: 'Some text 2.'
-		}
-	];
+	const [items, setItems] = useState(INITIAL_DATA);
+	const addItem = (item) => {
+		setItems((oldItems) => [
+			...oldItems,
+			{
+				text: item.text,
+				title: item.title,
+				date: new Date(item.date),
+				id: Math.max(...oldItems.map(({ id }) => id)) + 1
+			}
+		]);
+	};
+
+	const sortedItems = (a, b) => {
+		if (a.date < b.date) return 1;
+		return -1;
+	};
+
 	return (
 		<div className="app">
 			<LeftPanel>
 				<Header />
 				<JournalAddButton />
 				<JournalList>
-					<CardButton>
-						<JournalItem
-							title={data[0].title}
-							date={data[0].date}
-							text={data[0].text}
-						/>
-					</CardButton>
-					<CardButton>
-						<JournalItem
-							title={data[1].title}
-							date={data[1].date}
-							text={data[1].text}
-						/>
-					</CardButton>
+					{items.sort(sortedItems).map((el) => (
+						<CardButton key={el.id}>
+							<JournalItem title={el.title} date={el.date} text={el.text} />
+						</CardButton>
+					))}
 				</JournalList>
 			</LeftPanel>
 			<Body>
-				<JournalForm />
+				<JournalForm onSubmit={addItem} />
 			</Body>
 		</div>
 	);
